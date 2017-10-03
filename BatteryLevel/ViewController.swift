264344d70120715ce0670305828a80462ff19ee9
Maxime Moison <maximaxof@me.com>
2017-10-03 14:17:13 -0700

    Initial Commit

---
+++ BatteryLevel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteryLevel
+//
+//  Created by Maxime Moison on 10/3/17.
+//  Copyright © 2017 Maxime Moison. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
